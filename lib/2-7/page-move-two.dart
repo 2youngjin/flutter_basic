0170c4b313c52181ad8dd7b511d360a5eb056951
DESKTOP-C6DUT5T\bin <dudwls9418@wattsolution.co.kr>
2024-04-29 09:12:27 +0900

    (2-7) Flutter에서 화면 이동하기 - 네비게이터

--- lib/2-7/page-move-two.dart
+++ lib/2-7/page-move-two.dart
@@ -1,20 +1,10 @@
+import 'package:fastcampus_flutter_test/2-7/screen/2-7_screen/new_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-          title: const Text(
-            'Flutter에서 화면 이동하기 2',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.white,
-            ),
-          ),
-        ),
         body: Body(),
       ),
     ),
@@ -26,6 +16,32 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'Flutter에서 화면 이동하기 - 네비게이터',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+        child: TextButton(
+          child: const Text('Go to Page'),
+          onPressed: () {
+            //Navigator.pushReplacement( // Stack이 제거되어 이동하기 때문에 다음화면에서 뒤로가기 버튼 활성이 되지 않음
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const NewPage(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
